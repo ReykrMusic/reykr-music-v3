91be4bfd530106d49ad87ee8dda83834b0584d90
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-06 15:01:50 -0400

    updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,21 @@
 import Layout from '@/components/Layout'
+import { Roboto_Mono } from 'next/font/google'
+import Link from 'next/link'
+import { FaArrowLeft } from 'react-icons/fa'
+const roboto_mono = Roboto_Mono({ subsets: ['latin'] })
 
 const ErrorPage = () => {
   return (
     <Layout pageTitle="404">
-      <p>404 page</p>
+      <div className="text-white max-w-max text-center mx-auto mt-44">
+        <h1 className={`text-7xl ${roboto_mono.className}`}>404</h1>
+        <p className="my-8 text-2xl">Sorry, that page doesn&apos;t exist!</p>
+        <div>
+          <Link href="/" className="BackButton">
+            <FaArrowLeft className="BackButton__arrow" /> Go back
+          </Link>
+        </div>
+      </div>
     </Layout>
   )
 }
