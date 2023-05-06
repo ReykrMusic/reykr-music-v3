223c3b7fe0d1203520782fbf8f59370db4797228
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-06 12:13:38 -0400

    added animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,89 +10,102 @@ import {
 } from 'react-icons/fa'
 import { Spotify } from 'react-spotify-embed'
 import content from '../../public/content'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   const { profilePhoto, bio, spotifySongs } = content
   return (
     <Layout>
       <div>
-        <div className="Glass mx-auto max-w-xl mt-44">
-          <div className="p-6 sm:px-12 sm:pb-12 sm:pt-6 bg-white opacity-80">
-            <h1 className="text-center text-7xl">REYKR</h1>
-            <div className="mx-auto max-w-sm flex justify-center gap-20 text-2xl mt-6">
-              <button
-                className="hover:scale-110 focus:scale-110 transition-all ease-in-out duration-300"
-                onClick={() => scrollToElement('about')}
-              >
-                About
-              </button>
-              <button
-                className="hover:scale-110 focus:scale-110 transition-all ease-in-out duration-300"
-                onClick={() => scrollToElement('music')}
-              >
-                Music
-              </button>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.8 }}
+        >
+          <div className="Glass mx-auto max-w-xl mt-44">
+            <div className="p-6 sm:px-12 sm:pb-12 sm:pt-6 bg-white opacity-80">
+              <h1 className="text-center text-7xl">REYKR</h1>
+              <div className="mx-auto max-w-sm flex justify-center gap-20 text-2xl mt-6">
+                <button
+                  className="hover:scale-110 focus:scale-110 transition-all ease-in-out duration-300"
+                  onClick={() => scrollToElement('about')}
+                >
+                  About
+                </button>
+                <button
+                  className="hover:scale-110 focus:scale-110 transition-all ease-in-out duration-300"
+                  onClick={() => scrollToElement('music')}
+                >
+                  Music
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <ul className="text-white mx-auto max-w-max flex gap-8 mt-6 text-4xl">
-          <li>
-            <a
-              href="https://www.instagram.com/reykr.official/"
-              aria-label="Instagram"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:contrast-50 focus:contrast-50 transition"
-            >
-              <FaInstagram />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.facebook.com/reykr.music"
-              aria-label="Facebook"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:contrast-50 focus:contrast-50 transition"
-            >
-              <FaFacebook />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://open.spotify.com/artist/4i5oaaegWPdDi8p3Dm23rJ"
-              aria-label="Spotify"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:contrast-50 focus:contrast-50 transition"
-            >
-              <FaSpotify />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://soundcloud.com/reykr"
-              aria-label="SoundCloud"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:contrast-50 focus:contrast-50 transition"
-            >
-              <FaSoundcloud />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://reykr.bandcamp.com/"
-              aria-label="Bandcamp"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:contrast-50 focus:contrast-50 transition"
-            >
-              <FaBandcamp />
-            </a>
-          </li>
-        </ul>
-        <div className="Glass mx-auto my-[100vh]">
+          <ul className="text-white mx-auto max-w-max flex gap-8 mt-6 text-4xl">
+            <li>
+              <a
+                href="https://www.instagram.com/reykr.official/"
+                aria-label="Instagram"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:contrast-50 focus:contrast-50 transition"
+              >
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com/reykr.music"
+                aria-label="Facebook"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:contrast-50 focus:contrast-50 transition"
+              >
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://open.spotify.com/artist/4i5oaaegWPdDi8p3Dm23rJ"
+                aria-label="Spotify"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:contrast-50 focus:contrast-50 transition"
+              >
+                <FaSpotify />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://soundcloud.com/reykr"
+                aria-label="SoundCloud"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:contrast-50 focus:contrast-50 transition"
+              >
+                <FaSoundcloud />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://reykr.bandcamp.com/"
+                aria-label="Bandcamp"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:contrast-50 focus:contrast-50 transition"
+              >
+                <FaBandcamp />
+              </a>
+            </li>
+          </ul>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true, margin: '-150px' }}
+          transition={{ duration: 1.8 }}
+          className="Glass mx-auto my-[100vh]"
+        >
           <div
             id="about"
             className="text-black bg-white opacity-80 flex flex-col md:flex-row items-center gap-2 md:w-[700px] scroll-mt-12"
@@ -111,8 +124,15 @@ const Home = () => {
               <p className="text-lg">{bio}</p>
             </div>
           </div>
-        </div>
-        <div id="music" className="text-white mb-44 scroll-mt-12">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true, margin: '-150px' }}
+          transition={{ duration: 1.8 }}
+          id="music"
+          className="text-white mb-44 scroll-mt-12"
+        >
           <ul className="SpotifyGrid">
             {spotifySongs.map((song) => (
               <li key={song} className="flex justify-center">
@@ -120,7 +140,7 @@ const Home = () => {
               </li>
             ))}
           </ul>
-        </div>
+        </motion.div>
       </div>
     </Layout>
   )
