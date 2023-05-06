0411782d8d2cadd4dd809b8dc1f1766d78cdff8b
Lucas Silbernagel <lucas.silbernagel@gmail.com>
2023-05-06 17:18:04 -0400

    added Enter button

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,22 @@
 import { ReactNode } from 'react'
 import Seo from './Seo'
-import SmokeElement from 'smoke-effect-react'
-import { motion } from 'framer-motion'
 
 interface ILayoutProps {
   pageTitle?: string
   pageDescription?: string
   pageImage?: string
   children: ReactNode
+  hideFooter?: boolean
 }
 
 const Layout = (props: ILayoutProps) => {
-  const { pageTitle, pageDescription, pageImage, children } = props
+  const {
+    pageTitle,
+    pageDescription,
+    pageImage,
+    children,
+    hideFooter = false,
+  } = props
 
   return (
     <div className="relative">
@@ -20,34 +25,23 @@ const Layout = (props: ILayoutProps) => {
         pageDescription={pageDescription}
         pageImage={pageImage}
       />
-      <motion.div
-        className="fixed top-0"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1.8 }}
-      >
-        <SmokeElement
-          opacity="0"
-          src="/smoke-background.png"
-          smokeSrc="/smoke-element.png"
-          smokeOpacity="0.7"
-        />
-      </motion.div>
       <header></header>
       <main className="px-4">{children}</main>
-      <footer className="pb-4 relative">
-        <p className="text-center text-white">
-          Built by{' '}
-          <a
-            href="https://lucassilbernagel.com/"
-            target="_blank"
-            rel="noreferrer"
-            className="underline underline-offset-4 hover:underline-offset-2 focus:underline-offset-2 ease-in-out transition-all duration-300"
-          >
-            Lucas Silbernagel
-          </a>
-        </p>
-      </footer>
+      {!hideFooter && (
+        <footer className="pb-4 relative">
+          <p className="text-center text-white">
+            Built by{' '}
+            <a
+              href="https://lucassilbernagel.com/"
+              target="_blank"
+              rel="noreferrer"
+              className="underline underline-offset-4 hover:underline-offset-2 focus:underline-offset-2 ease-in-out transition-all duration-300"
+            >
+              Lucas Silbernagel
+            </a>
+          </p>
+        </footer>
+      )}
     </div>
   )
 }
